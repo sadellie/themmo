0389ac41287992f3fe507cc92bf8c679238c5eac
Sad Ellie <openthiswhitedoor@gmail.com>
2022-09-03 13:46:44 +0300

    0.0.5

--- themmosample/build.gradle.kts
+++ themmosample/build.gradle.kts
@@ -53,10 +53,10 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.compose.ui:ui:1.3.0-alpha03")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.3.0-alpha03")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.3.0-alpha03")
-    implementation("androidx.compose.material3:material3:1.0.0-alpha16")
+    implementation("androidx.compose.ui:ui:1.3.0-beta01")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.0-beta01")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.3.0-beta01")
+    implementation("androidx.compose.material3:material3:1.0.0-beta01")
     implementation("androidx.activity:activity-compose:1.5.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
