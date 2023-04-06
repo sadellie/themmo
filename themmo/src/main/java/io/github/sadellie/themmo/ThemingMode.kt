7f284bbc65dfa08294238d7cc68a384b2f1f4d28
Sad Ellie <openthiswhitedoor@gmail.com>
2023-04-06 22:39:27 +0300

    Move ThemingMode out of ThemmoController.kt

---
+++ themmo/src/main/java/io/github/sadellie/themmo/ThemingMode.kt
@@ -0,0 +1,18 @@
+package io.github.sadellie.themmo
+
+enum class ThemingMode {
+    /**
+     * Will ask system which theme to use.
+     */
+    AUTO,
+
+    /**
+     * Will use light colors only.
+     */
+    FORCE_LIGHT,
+
+    /**
+     * Will use dark colors only.
+     */
+    FORCE_DARK
+}
--- themmo/src/main/java/io/github/sadellie/themmo/ThemmoController.kt
+++ themmo/src/main/java/io/github/sadellie/themmo/ThemmoController.kt
@@ -159,23 +159,6 @@ class ThemmoController(
 
 }
 
-enum class ThemingMode {
-    /**
-     * Will ask system which theme to use.
-     */
-    AUTO,
-
-    /**
-     * Will use light colors only.
-     */
-    FORCE_LIGHT,
-
-    /**
-     * Will use dark colors only.
-     */
-    FORCE_DARK
-}
-
 /**
  * Restores [ThemmoController] for saver.
  *
